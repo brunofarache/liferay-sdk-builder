3c021c14aac1e64a868ec99338875f1eac32c973
brunofarache <bruno.farache@liferay.com>
2016-07-22 12:40:02 -0300

    Adding generated files back

---
+++ gen/com/liferay/mobile/sdk/v7/theme/ThemeService.java
@@ -0,0 +1,16 @@
+package com.liferay.mobile.sdk.v7.theme;
+
+import com.liferay.mobile.sdk.Call;
+import com.liferay.mobile.sdk.annotation.Param;
+import com.liferay.mobile.sdk.annotation.Path;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+@Path("/theme")
+public interface ThemeService {
+  @Path("/get-themes")
+  Call<JSONArray> getThemes(@Param(name = "companyId") long companyId);
+
+  @Path("/get-war-themes")
+  Call<JSONObject> getWARThemes();
+}
